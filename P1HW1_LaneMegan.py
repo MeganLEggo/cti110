2b808954e6a1d641b8c4100919047152a1ad6dae
MeganLEggo <159936124+MeganLEggo@users.noreply.github.com>
2024-02-15 11:47:39 -0500

    Add files via upload
    Numbers! Squares, Cubes, and pick 2 inputs with outputs.

---
+++ P1HW1_LaneMegan.py
@@ -0,0 +1,21 @@
+# Megan Lane
+
+# 2/15/2024
+
+# P1HW1
+
+# Basic Output with Variables
+
+user_num = int(input('Enter integer:\n'))
+
+print('You entered:', user_num)
+user_num_sq = user_num * user_num
+print(user_num, 'squared is', user_num_sq)
+user_num_cb = user_num * user_num * user_num
+print ('And', user_num, 'cubed is', user_num_cb, "!!")
+new_num = int(input('Enter another integer:\n'))
+
+new_num_add = user_num + new_num
+print(user_num, "+", new_num, "is", new_num_add)
+new_num_mlt = user_num * new_num
+print(user_num, "*", new_num, "is", new_num_mlt)
