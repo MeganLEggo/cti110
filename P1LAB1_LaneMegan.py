856c5f53cee8425bb30dfd2ca66a1d017392a0f6
MeganLEggo <159936124+MeganLEggo@users.noreply.github.com>
2024-02-15 10:20:28 -0500

    Add files via upload

---
+++ P1LAB1_LaneMegan.py
@@ -0,0 +1,17 @@
+# Megan Lane
+
+# 2/13/2024
+
+# P1LAB1
+
+# In my project we learn input and outputs.
+
+
+
+#CREATE A VAR AND GET INPUT FROM A USER
+
+F_name = input("What is your name? Enter here: ")
+
+#OUTPUT A MESSAGE USING THE VARIABLE
+
+print("Hey", F_name, "\nWelcome to zyBooks!")
